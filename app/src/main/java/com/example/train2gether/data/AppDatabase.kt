144a010f3d7529dde98e752b4a2e7bf795fd3bbc
Mattia <mattia.capretti08@gmail.com>
2026-08-20 11:38:45 +0200

    creazione AppDatabase

--- app/src/main/java/com/example/train2gether/data/AppDatabase.kt
+++ app/src/main/java/com/example/train2gether/data/AppDatabase.kt
@@ -1,4 +1,19 @@
 package com.example.train2gether.data
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        Esercizio::class,
+        Scheda::class,
+        EsercizioScheda::class,
+        SeriePrevista::class,
+        Allenamento::class,
+        SerieEseguita::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase: RoomDatabase() {
 }
