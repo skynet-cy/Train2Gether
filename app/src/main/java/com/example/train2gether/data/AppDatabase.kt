f83440f0f079a66c5eae7118aa26b8b9fbebe213
Mattia <mattia.capretti08@gmail.com>
2026-08-20 15:26:17 +0200

    aggiunta del singleton

--- app/src/main/java/com/example/train2gether/data/AppDatabase.kt
+++ app/src/main/java/com/example/train2gether/data/AppDatabase.kt
@@ -2,6 +2,8 @@ package com.example.train2gether.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
 
 @Database(
     entities = [
@@ -18,5 +20,25 @@ import androidx.room.RoomDatabase
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun esercizioDao(): EsercizioDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "train2gether_database"
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
 
 }
